b76d94950d77f731c3575324ffb5c5240cc21dd6
Jasm <joaovictorasm203@gmail.com>
2026-05-21 21:55:28 -0300

    Create procedure to transfer pet ownership

---
+++ prcedures/transferirpet.sql
@@ -0,0 +1,23 @@
+create or replace procedure transferir_pet(p_id integer, c_id integer)
+language plpgsql
+as $$
+begin
+    if not exists (select 1 from clientes where id = c_id) then 
+        raise exception 'Cliente com id % não existe.', c_id;
+    end if;
+
+    if not exists (select 1 from pets where id = p_id) then
+        raise exception 'Pet com id % não encontrado.', p_id;
+    end if;
+
+    if exists (select 1 from pets where id = p_id and cliente_id = c_id) then 
+        raise exception 'O cliente % já é dono do pet %.', c_id, p_id;
+    end if;
+
+    update pets
+    set cliente_id = c_id
+    where id = p_id;
+
+    raise notice 'Pet % alterado com sucesso', p_id;
+end;
+$$;
